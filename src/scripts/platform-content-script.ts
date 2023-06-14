999922e86585ca3cc6d99bf47fe4d903cb5d438d
nbolam <nbolam59@gmail.com>
2023-06-14 13:00:37 -0700

    Force updates off on page load

--- src/scripts/platform-content-script.ts
+++ src/scripts/platform-content-script.ts
@@ -1,7 +1,7 @@
 import { TabMessage } from "../tab-messsage";
 import { initializeModels } from "./tf-models";
 import { toggle } from "./update-volume";
-import { ENABLE_UPDATES_ON_LOAD } from "../dev-flags";
+import { DISABLE_UPDATES_ON_LOAD } from "../dev-flags";
 
 async function main() {
   // Initialize CV models
@@ -10,8 +10,8 @@ async function main() {
   // Initialize this as the "active" tab for the extension
   chrome.runtime.sendMessage(TabMessage.Init);
 
-  // Toggle volume updates
-  if (ENABLE_UPDATES_ON_LOAD) toggle(true);
+  // Force status updates to off state
+  if (DISABLE_UPDATES_ON_LOAD) toggle(true);
 }
 
 chrome.runtime.onMessage.addListener((message) => {
--- test/dev-flags.test.ts
+++ test/dev-flags.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@jest/globals";
 import {
   ENABLE_VERBOSE_LOGGING,
-  ENABLE_UPDATES_ON_LOAD,
+  DISABLE_UPDATES_ON_LOAD,
   UPDATE_INTERVAL_MILLIS,
 } from "../src/dev-flags";
 
@@ -10,6 +10,6 @@ import {
  */
 test("dev flags are disabled", () => {
   expect(ENABLE_VERBOSE_LOGGING).toBe(false);
-  expect(ENABLE_UPDATES_ON_LOAD).toBe(true);
+  expect(DISABLE_UPDATES_ON_LOAD).toBe(true);
   expect(UPDATE_INTERVAL_MILLIS).toBe(1500);
 });
